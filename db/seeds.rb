ea3136516add36c6050de1f5c90ec0c72723680b
Pauline Poize <pauline.poize@kedgebs.com>
2020-05-26 12:45:45 +0200

    Exo articles done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |_variable|
+  Article.create(
+  	title: Faker::FunnyName.name,
+  	content: Faker::Lorem.paragraph
+  	)
+end
